209ae0f395520d7182b68c327bc4fa2c196b5d46
claussen100 <claussenpatrick@gmail.com>
2021-02-26 01:14:32 -0300

    nps database

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,11 @@
+import 'reflect-metadata'
 import express from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PACTH => Alteração específica
- */
-
-// http://localhost:3333/users
-
-app.get("/", (request, response) => {
-
-    return response.json({ message: "Hello World - NLW04" });
-})
-
-//1 parametro => Rota(Recurso API)
-//2 parametro => Request, response
-
-app.post("/", (request, response) => {
-    //ja recebeu os dados pra salvar
-
-    return response.json({ message: "Os dados foram salvados com sucesso!" });
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is Running!"));
